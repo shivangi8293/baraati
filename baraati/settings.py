f20868eb40175e81ae7ac91240d541f4fe97f2eb
shivangi8293 <[shivangi8293@gmail.com]>
2017-04-26 03:06:54 +0530

    dfdf

--- baraati/settings.py
+++ baraati/settings.py
@@ -151,7 +151,7 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = 'https://s3-%s.amazonaws.com/%s/media/' % (AWS_REGION, AWS_STORAGE_BUCKET_NAME)
+MEDIA_URL = 'https://s3.%s.amazonaws.com/%s/' % (AWS_REGION, AWS_STORAGE_BUCKET_NAME)
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 from herokuify.aws import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME
