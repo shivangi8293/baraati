dc90b24e883d0127c02570c72bcc8d0894c1b3ac
shivangi8293 <[shivangi8293@gmail.com]>
2017-04-26 00:34:45 +0530

    dfdsf

--- baraati/settings.py
+++ baraati/settings.py
@@ -22,6 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'q39wx34&&z-s4x^9$g(obab!&twgtk56nnl$xi+faksc-78(!7'
 
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MEDIA_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
