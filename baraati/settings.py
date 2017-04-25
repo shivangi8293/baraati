afb03eb996013b107496bebcee99bfa1acdb474b
shivangi8293 <[shivangi8293@gmail.com]>
2017-04-26 01:42:02 +0530

    fdgd

--- baraati/settings.py
+++ baraati/settings.py
@@ -146,11 +146,12 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 AWS_QUERYSTRING_AUTH = False
+AWS_REGION = os.environ.get('AWS_REGION')  # e.g. eu-west-1 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = 'https://s3-%s.amazonaws.com/%s/media/' % (AWS_REGION, AWS_STORAGE_BUCKET_NAME)
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 import dj_database_url
